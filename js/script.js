5a602c9d126f03f28d461628b6e399f8511a828a
Praveenkum12 <praveenkum1225@gmail.com>
2022-06-21 13:51:24 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,31 +1,26 @@
+const normal = document.querySelector(".header");
+normal.classList.remove("nav-open");
 
-const normal = document.querySelector('.header');
-normal.classList.remove('nav-open');
+const menu = document.querySelector(".nav-1");
+const close = document.querySelector(".nav-2");
+const main = document.querySelector(".main-nav");
 
-const menu = document.querySelector('.nav-1');
-const close = document.querySelector('.nav-2');
-const main = document.querySelector('.main-nav');
+menu.addEventListener("click", function () {
+  normal.classList.add("nav-open");
+  menu.style.display = "none";
+  close.style.display = "block";
 
-
-menu.addEventListener('click', function () {
-  normal.classList.add('nav-open');
-  menu.style.display = 'none';
-  close.style.display = 'block';
-
-  document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape') {
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
       callMe();
     }
-  })
+  });
 });
 
 const callMe = function () {
-  normal.classList.remove('nav-open');
-  menu.style.display = 'block';
-  close.style.display = 'none';
+  normal.classList.remove("nav-open");
+  menu.style.display = "block";
+  close.style.display = "none";
 };
 
-close.addEventListener('click', callMe);
-
-
-
+close.addEventListener("click", callMe);
