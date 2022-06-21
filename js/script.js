292bca69f8cf275b602cae073d65bddab180811a
Praveenkum12 <praveenkum1225@gmail.com>
2022-06-21 13:45:52 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+const normal = document.querySelector('.header');
+normal.classList.remove('nav-open');
+
+const menu = document.querySelector('.nav-1');
+const close = document.querySelector('.nav-2');
+const main = document.querySelector('.main-nav');
+
+
+menu.addEventListener('click', function () {
+  normal.classList.add('nav-open');
+  menu.style.display = 'none';
+  close.style.display = 'block';
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+      callMe();
+    }
+  })
+});
+
+const callMe = function () {
+  normal.classList.remove('nav-open');
+  menu.style.display = 'block';
+  close.style.display = 'none';
+};
+
+close.addEventListener('click', callMe);
+
+
+
